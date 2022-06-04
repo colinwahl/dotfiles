af5e8663c857bb3043559cec40aefca4133000f6
Colin Wahl <colin.t.wahl@gmail.com>
2022-06-03 18:52:27 -0700

    Initial nvim config

---
+++ .config/nvim/lua/user/lsp/settings/purescriptls.lua
@@ -0,0 +1,11 @@
+return {
+  settings = {
+    purescript = {
+      addSpagoSources = true,
+      autoStartPscIde = true,
+    },
+  },
+  flags = {
+    debounce_text_changes = 150;
+  },
+}
---
+++ .config/nvim/lua/user/lsp/settings/rustanalyzer.lua
@@ -0,0 +1,5 @@
+local opts = {
+  cmd = { "rustup", "run", "nightly", "rust-analyzer" },
+}
+
+return opts
